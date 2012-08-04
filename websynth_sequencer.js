e769d1ccb58862527a04e3cc2cfb0aee76800f66
Bill Eager <beager@gmail.com>
2012-08-04 01:08:21 -0400

    Enable keyboard input for the synthesizer

--- websynth_sequencer.js
+++ websynth_sequencer.js
@@ -30,6 +30,35 @@ $(function() {
 		n8, n8, n8, n8, n8, n8, n8, n8
 	];
 
+	var key_note_mappings = {
+		90: 7,
+		83: 8,
+		88: 9,
+		68: 10,
+		67: 11,
+		86: 12,
+		71: 13,
+		66: 14,
+		72: 15,
+		78: 16,
+		74: 17,
+		77: 18,
+		188: 19,
+		81: 19,
+		50: 20,
+		87: 21,
+		51: 22,
+		69: 23,
+		82: 24,
+		53: 25,
+		84: 26,
+		54: 27,
+		89: 28,
+		55: 29,
+		85: 30,
+		73: 31,
+	}
+
 	$('<img />').switch({
 		id: 'play_button', image: 'images/play_button.png',
 		left: 350, top: 500, width: 36, height: 36, value: 0,
@@ -55,5 +84,50 @@ $(function() {
 			setTimeout(function() {seq(pos + 1);}, sequence_duration[pos]);
 		}
 	};
+
+	var down_key_stack = [];
+
+	$('body').keydown(function(event) {
+		var note = key_note_mappings[event.which];
+
+		if (note)
+		{
+			if (down_key_stack.indexOf(note) == -1)
+			{
+				down_key_stack.push(note);
+				p.stop();
+				seq_note_on(note);
+			}
+		}
+	});
+
+	$('body').keyup(function(event) {
+		var note = key_note_mappings[event.which];
+
+		if (note && (down_key_stack.indexOf(note) > -1))
+		{
+			if (down_key_stack[down_key_stack.length-1] == note)
+			{
+				// the key up was the current key playing, turn it off
+				seq_note_off(note);
+				if (down_key_stack.length > 1)
+				{
+					// there's another note on, it needs to be played
+					new_note_needed = true;
+					seq_note_on(down_key_stack[down_key_stack.length-2]);
+				}
+			}
+			down_key_stack.splice(down_key_stack.indexOf(note));
+		}
+		//super failsafe
+		if (down_key_stack.length == 0)
+		{
+			p.stop();
+			for (var i = 0; i < 42; i++)
+			{
+				$('#key_' + i).keypad("value", 0);
+			}
+		}
+	});
 
 });
